5f3644bc60a354a11ce8c21118f1b58d53d094e4
alxkai <alex@instalily.ai>
2023-09-13 18:42:12 -0400

    first commit

--- next.config.js
+++ next.config.js
@@ -1,22 +1,6 @@
-// const { withContentlayer } = require('next-contentlayer')
+const { withContentlayer } = require('next-contentlayer')
 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {}
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-// module.exports = withContentlayer(nextConfig)
-
-const { withContentlayer } = require('next-contentlayer');
-
-module.exports = withContentlayer({
-  contentlayer: {
-    // Configuration for next-contentlayer
-    contentDir: 'content', // Path to your content directory
-  },
-  // Your other Next.js configurations go here
-  // For example, you can define your target, webpack config, or other Next.js options here.
-});
-// next.config.js
-module.exports = {
-    distDir: 'out',
-    // Your other Next.js configuration options...
-  };
+module.exports = withContentlayer(nextConfig)
