34e13b774dea7eda001a4c0b173c2b63a360bf8c
alxkai <alex@instalily.ai>
2023-09-14 10:03:27 -0400

    team

--- components/team.tsx
+++ components/team.tsx
@@ -16,8 +16,8 @@ export default function Team() {
         <div className="py-12 md:py-20 border-t border-gray-800">
 
           {/* Section header */}
-          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20 GraebenbachMonoMed text-[#F2F2F2]">
-            <h2 className="h2 mb-4">Our passionate team</h2>
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20 GraebenbachMono text-[#F2F2F2]">
+            <h2 className="h2 mb-4">OUR PASSIONATE TEAM</h2>
             <p className="text-xl text-gray-400">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consequat.</p>
           </div>
 
