f0a62c1b901695267bb858aab57f0f20dc57f7fb
alxkai <alex@instalily.ai>
2023-09-15 08:36:03 -0400

    Update career.tsx

--- components/career.tsx
+++ components/career.tsx
@@ -1,116 +1,85 @@
 export default function Career() {
   return (
     <section className="bg-[#F2F2F2]">
-      <div className="max-w-4xl mx-auto px-4 sm:px-6 ">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 ">
         <div className="pb-12 md:pb-20">
 
           {/* Section header */}
-          <h3 className="h3 mb-8 pt-8">Careers</h3>
+          <h3 className="h3 mb-8 pt-8">Current Openings 
+          <br/>
+          <span className="flex justify-between items-center text-xl font-bold text-gray-500 transition duration-150 pt-2 ease-in-out">We are always on the lookout for talented individuals to join our team in various roles, including:</span></h3>
 
           {/* Job list */}
           <div className="-my-3" data-aos-id-career>
 
-            {/* 1st Item */}
             <div className="py-3" data-aos="fade-up" data-aos-anchor="[data-aos-id-career]">
               <div className="pb-6 border-b border-gray-800">
                 <div className="mb-2">
-                  <a className="flex justify-between items-center text-xl font-bold text-[#181818] hover:text-[#D9027D] transition duration-150 ease-in-out" href="#0">
-                    <span>Lead Product Designer</span>
-                    <svg className="stroke-current shrink-0 ml-2" width="24" height="24" xmlns="http://www.w3.org/2000/svg">
-                      <g strokeWidth="2" fill="none">
-                        <path d="M1 12h20" />
-                        <path strokeLinecap="square" d="M14 5l7 7-7 7" />
-                      </g>
-                    </svg>
-                  </a>
-                </div>
-                <div className="text-lg text-gray-400 mb-3">
-                  You'll be joing the Experience Design Team as a Lead Product Designer. You'll interface daily with developers - encouraging and advancing best practices in software.
-                </div>
-                <div className="text-gray-400 flex flex-wrap items-center">
-                  <div className="inline-flex items-center">
-                    <svg className="mr-2 shrink-0" width="14" height="17" viewBox="0 0 14 17" xmlns="http://www.w3.org/2000/svg">
-                      <path className="fill-current text-gray-300" d="M7 0C3.1 0 0 3.1 0 7c0 1.9.7 3.7 2.1 5 .1.1 4.1 3.7 4.2 3.8.4.3 1 .3 1.3 0 .1-.1 4.2-3.7 4.2-3.8 1.4-1.3 2.1-3.1 2.1-5 .1-3.9-3-7-6.9-7zm0 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z" />
-                    </svg>
-                    <span>Anywhere (EU timezone)</span>
-                  </div>
-                  <span className="text-gray-700 mx-3"> - </span>
-                  <div className="inline-flex items-center">
-                    <svg className="mr-2 shrink-0" width="14" height="16" viewBox="0 0 14 16" xmlns="http://www.w3.org/2000/svg">
-                      <path className="fill-current text-gray-300" d="M9.5 3L11 0H3l1.5 3zM10 5H4c-2 1.458-4 4.235-4 6.4C0 12.573.5 16 7 16c6.495 0 7-3.431 7-4.6 0-2.165-2-4.942-4-6.4z" />
-                    </svg>
-                    <span>$70k - $120k</span>
-                  </div>
+                    <span className="flex justify-between items-center text-xl font-bold text-[#181818] transition duration-150 ease-in-out">Software Engineers (Frontend, Backend, Full Stack)</span>
                 </div>
               </div>
             </div>
 
-            {/* 2nd Item */}
-            <div className="py-3" data-aos="fade-up" data-aos-delay="200" data-aos-anchor="[data-aos-id-career]">
+            <div className="py-3" data-aos="fade-up" data-aos-anchor="[data-aos-id-career]">
               <div className="pb-6 border-b border-gray-800">
                 <div className="mb-2">
-                  <a className="flex justify-between items-center text-xl font-bold text-[#181818] hover:text-[#D9027D] transition duration-150 ease-in-out" href="#0">
-                    <span>Front-end Engineer</span>
-                    <svg className="stroke-current shrink-0 ml-2" width="24" height="24" xmlns="http://www.w3.org/2000/svg">
-                      <g strokeWidth="2" fill="none">
-                        <path d="M1 12h20" />
-                        <path strokeLinecap="square" d="M14 5l7 7-7 7" />
-                      </g>
-                    </svg>
-                  </a>
-                </div>
-                <div className="text-lg text-gray-400 mb-3">
-                  You'll be joing the Experience Design Team as a Lead Product Designer. You'll interface daily with developers - encouraging and advancing best practices in software.
-                </div>
-                <div className="text-gray-400 flex flex-wrap items-center">
-                  <div className="inline-flex items-center">
-                    <svg className="mr-2 shrink-0" width="14" height="17" viewBox="0 0 14 17" xmlns="http://www.w3.org/2000/svg">
-                      <path className="fill-current text-gray-300" d="M7 0C3.1 0 0 3.1 0 7c0 1.9.7 3.7 2.1 5 .1.1 4.1 3.7 4.2 3.8.4.3 1 .3 1.3 0 .1-.1 4.2-3.7 4.2-3.8 1.4-1.3 2.1-3.1 2.1-5 .1-3.9-3-7-6.9-7zm0 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z" />
-                    </svg>
-                    <span>Anywhere (EU timezone)</span>
-                  </div>
-                  <span className="text-gray-700 mx-3"> - </span>
-                  <div className="inline-flex items-center">
-                    <svg className="mr-2 shrink-0" width="14" height="16" viewBox="0 0 14 16" xmlns="http://www.w3.org/2000/svg">
-                      <path className="fill-current text-gray-300" d="M9.5 3L11 0H3l1.5 3zM10 5H4c-2 1.458-4 4.235-4 6.4C0 12.573.5 16 7 16c6.495 0 7-3.431 7-4.6 0-2.165-2-4.942-4-6.4z" />
-                    </svg>
-                    <span>$70k - $120k</span>
-                  </div>
+                    <span className="flex justify-between items-center text-xl font-bold text-[#181818] transition duration-150 ease-in-out">AI and Machine Learning Engineers</span>
                 </div>
               </div>
             </div>
 
-            {/* 3rd Item */}
-            <div className="py-3" data-aos="fade-up" data-aos-delay="400" data-aos-anchor="[data-aos-id-career]">
+            <div className="py-3" data-aos="fade-up" data-aos-anchor="[data-aos-id-career]">
               <div className="pb-6 border-b border-gray-800">
                 <div className="mb-2">
-                  <a className="flex justify-between items-center text-xl font-bold text-[#181818] hover:text-[#D9027D] transition duration-150 ease-in-out" href="#0">
-                    <span>Product Manager</span>
-                    <svg className="stroke-current shrink-0 ml-2" width="24" height="24" xmlns="http://www.w3.org/2000/svg">
-                      <g strokeWidth="2" fill="none">
-                        <path d="M1 12h20" />
-                        <path strokeLinecap="square" d="M14 5l7 7-7 7" />
-                      </g>
-                    </svg>
-                  </a>
+                    <span className="flex justify-between items-center text-xl font-bold text-[#181818] transition duration-150 ease-in-out">Data Scientists</span>
                 </div>
-                <div className="text-lg text-gray-400 mb-3">
-                  You'll be joing the Experience Design Team as a Lead Product Designer. You'll interface daily with developers - encouraging and advancing best practices in software.
+              </div>
+            </div>
+
+            <div className="py-3" data-aos="fade-up" data-aos-anchor="[data-aos-id-career]">
+              <div className="pb-6 border-b border-gray-800">
+                <div className="mb-2">
+                    <span className="flex justify-between items-center text-xl font-bold text-[#181818] transition duration-150 ease-in-out">Product Managers</span>
                 </div>
-                <div className="text-gray-400 flex flex-wrap items-center">
-                  <div className="inline-flex items-center">
-                    <svg className="mr-2 shrink-0" width="14" height="17" viewBox="0 0 14 17" xmlns="http://www.w3.org/2000/svg">
-                      <path className="fill-current text-gray-300" d="M7 0C3.1 0 0 3.1 0 7c0 1.9.7 3.7 2.1 5 .1.1 4.1 3.7 4.2 3.8.4.3 1 .3 1.3 0 .1-.1 4.2-3.7 4.2-3.8 1.4-1.3 2.1-3.1 2.1-5 .1-3.9-3-7-6.9-7zm0 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z" />
-                    </svg>
-                    <span>Anywhere (EU timezone)</span>
-                  </div>
-                  <span className="text-gray-700 mx-3"> - </span>
-                  <div className="inline-flex items-center">
-                    <svg className="mr-2 shrink-0" width="14" height="16" viewBox="0 0 14 16" xmlns="http://www.w3.org/2000/svg">
-                      <path className="fill-current text-gray-300" d="M9.5 3L11 0H3l1.5 3zM10 5H4c-2 1.458-4 4.235-4 6.4C0 12.573.5 16 7 16c6.495 0 7-3.431 7-4.6 0-2.165-2-4.942-4-6.4z" />
-                    </svg>
-                    <span>$70k - $120k</span>
-                  </div>
+              </div>
+            </div>
+
+            <div className="py-3" data-aos="fade-up" data-aos-anchor="[data-aos-id-career]">
+              <div className="pb-6 border-b border-gray-800">
+                <div className="mb-2">
+                    <span className="flex justify-between items-center text-xl font-bold text-[#181818] transition duration-150 ease-in-out">UX/UI Designers</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="py-3" data-aos="fade-up" data-aos-anchor="[data-aos-id-career]">
+              <div className="pb-6 border-b border-gray-800">
+                <div className="mb-2">
+                    <span className="flex justify-between items-center text-xl font-bold text-[#181818] transition duration-150 ease-in-out">Sales and Business Development</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="py-3" data-aos="fade-up" data-aos-anchor="[data-aos-id-career]">
+              <div className="pb-6 border-b border-gray-800">
+                <div className="mb-2">
+                    <span className="flex justify-between items-center text-xl font-bold text-[#181818] transition duration-150 ease-in-out">Marketing and Communications</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="py-3" data-aos="fade-up" data-aos-anchor="[data-aos-id-career]">
+              <div className="pb-6 border-b border-gray-800">
+                <div className="mb-2">
+                    <span className="flex justify-between items-center text-xl font-bold text-[#181818] transition duration-150 ease-in-out">Customer Success and Support</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="py-3" data-aos="fade-up" data-aos-anchor="[data-aos-id-career]">
+              <div className="pb-6 border-b border-gray-800">
+                <div className="mb-2">
+                    <span className="flex justify-between items-center text-xl font-bold text-[#181818] transition duration-150 ease-in-out">Operations and Finance</span>
                 </div>
               </div>
             </div>
